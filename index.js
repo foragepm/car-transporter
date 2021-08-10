274a217f8dee5da998de84371408f3dde27af57b
Andrew Nesbitt <andrewnez@gmail.com>
2021-08-10 16:43:02 +0100

    timeout if request takes longer than 30 seconds

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 const http = require('http')
 const fetch = require('node-fetch');
 const url = require('url');
+const AbortController = require('abort-controller');
 
-var token = process.env.WEB3_TOKEN
-var ipfs_api = process.env.IPFS_API
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEIxMGRENDhCNkIzOTNhNzNiMzc4Y2E1MDBBNzliQjFjNENhQTE4NGIiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2MjgyNTM0MTA2NDEsIm5hbWUiOiJnaC1hcmNoaXZlciJ9.BTpo6I9jeWtfRh95_Q65C8yv-3E24EfL62pzHJOyr5Y'//process.env.WEB3_TOKEN
+var ipfs_api = 'http://51.15.3.135:5001' //process.env.IPFS_API
 
 async function transport(cid, filename) {
   if(!cid){ return {} }
   console.log('Uploading', cid, filename)
   var url = `${ipfs_api}/api/v0/dag/export?arg=${cid}&carversion=1`
   var download = await fetch(url, { method: 'POST' })
-  var upload = await fetch(`https://api.web3.storage/car`, {
-    method: 'POST',
-    headers: {
-        "Authorization": `Bearer ${token}`,
-        "x-name": filename
-    },
-    body: download.body
-  })
-  var json = await upload.json()
-  if (json.cid){
-    console.log(json)
-  } else {
-    var error = json
-    error.cid = cid
-    error.filename = filename
-    console.error(JSON.stringify(error))
+
+  const controller = new AbortController();
+  const timeout = setTimeout(() => {
+  	controller.abort();
+  }, 1000*30);
+
+  try {
+    var upload = await fetch(`https://api.web3.storage/car`, {
+      method: 'POST',
+      headers: {
+          "Authorization": `Bearer ${token}`,
+          "x-name": filename
+      },
+      body: download.body,
+      signal: controller.signal
+    })
+    var json = await upload.json()
+    if (json.cid){
+      console.log(json)
+    } else {
+      var error = json
+      error.cid = cid
+      error.filename = filename
+      console.error(JSON.stringify(error))
+    }
+    return json
+  } catch (error) {
+  	console.log('timeout error');
+    return {}
+  } finally {
+  	clearTimeout(timeout);
   }
-  return json
 }
 
 const handler = async function (req, res) {
